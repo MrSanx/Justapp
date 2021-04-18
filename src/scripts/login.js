f1cf1f0455b3b0fc1572a934d601be69f58a32eb
Nicolas Suarez <nicolasuarez24@gmail.com>
2021-04-17 23:27:05 -0500

    funcion de login/logout

---
+++ src/scripts/login.js
@@ -0,0 +1,14 @@
+import {auth} from './init.js';
+//login
+const loginForm = document.querySelector('#login-form');
+loginForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    //get user info
+    const email = loginForm['login-email'].value;
+    const password = loginForm['login-password'].value;
+
+    auth.signInWithEmailAndPassword(email, password).then(cred =>{
+        console.log('user logged in');
+        window.location.href = '../webapp/index.html';
+    });
+});
--- src/scripts/signupClient.js
+++ src/scripts/signupClient.js
@@ -32,16 +32,6 @@ signupForm.addEventListener('submit', (e) => {
 });
 
 
-//logout
-const logout = document.querySelector('#logout');
-logout.addEventListener('click', (e) => {
-    e.preventDefault();
-    console.log('este boton si sirve lptm');
-    //auth.signOut().then(() => {
-    //    console.log('logged out');
-    //});
-});
-
 /*
 //login
 const loginForm = document.querySelector('INSERTAR EL ID DEL FORM DE LOGIN');
