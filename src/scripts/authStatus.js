12313984de92e797cc70b49b00661ea73cac2cd5
Nicolas Suarez <nicolasuarez24@gmail.com>
2021-04-18 22:42:38 -0500

    Add email del usuario loggeado al menu de perfil

--- src/scripts/authStatus.js
+++ src/scripts/authStatus.js
@@ -1,7 +1,6 @@
 import {auth} from './init.js';
 auth.onAuthStateChanged(user => {
     if (user) {
-        console.log('user logged in:', user);
         setupUI(user);
     } else {
         setupUI(null);
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,13 +1,19 @@
 const loggedOutLinks = document.querySelectorAll('.logged-out');
 const loggedInLinks = document.querySelectorAll('.logged-in');
-const accountDetails = document.querySelector('.menu-email');
+const menuEmail = document.querySelector('.menu-email');
 
 
 const setupUI = (user) => {
     if(user){
+        //account info
+        const htmlEmail = `<div>${user.email}</div>`;
+        menuEmail.innerHTML = htmlEmail;
+        //toggle UI elements
         loggedInLinks.forEach(item => item.style.display = 'block');
-        loggedOutLinks.forEach(item => item.style.display ='none');
+        loggedOutLinks.forEach(item => item.style.display ='none'); 
     } else {
+        //hide user info
+        menuEmail.innerHTML = '';
         loggedInLinks.forEach(item => item.style.display = 'none');
         loggedOutLinks.forEach(item => item.style.display ='block');
     }
