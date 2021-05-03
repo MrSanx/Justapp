7964f224dd91082530f4a1db6c9ad77a1c20cc34
Nicolas Suarez <nicolasuarez24@gmail.com>
2021-05-03 16:28:52 -0500

    añadir informacion al perfil

--- src/scripts/authStatus.js
+++ src/scripts/authStatus.js
@@ -1,4 +1,5 @@
 import {auth} from './init.js';
+import {setupUI} from './index.js'
 auth.onAuthStateChanged(user => {
     if (user) {
         setupUI(user);
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,13 +1,25 @@
+import {db} from './init.js';
 const loggedOutLinks = document.querySelectorAll('.logged-out');
 const loggedInLinks = document.querySelectorAll('.logged-in');
 const menuEmail = document.querySelector('.menu-email');
+const userName = document.querySelector('#user-name');
 
 
-const setupUI = (user) => {
+export const setupUI = (user) => {
     if(user){
         //account info
-        const htmlEmail = `<div>${user.email}</div>`;
-        menuEmail.innerHTML = htmlEmail;
+        db.collection('clients').doc(user.uid).get().then(doc => {
+            const htmlEmail = `
+                <div>${user.email}</div>
+            `;
+            const htmlUserName = `
+                <div>${doc.data().name + ' ' + doc.data().lastName}</div>
+            `;
+            menuEmail.innerHTML = htmlEmail;
+            userName.innerHTML = htmlUserName;
+        }).catch(err => {
+            console.error()
+        })
         //toggle UI elements
         loggedInLinks.forEach(item => item.style.display = 'block');
         loggedOutLinks.forEach(item => item.style.display ='none'); 
