06f44b2826f2f670f77b448807df81a288317f08
nataliajj07 <82399387+nataliajj07@users.noreply.github.com>
2021-05-03 17:53:21 -0500

    Nuevo formulario abogado y boton en directorio

--- src/scripts/directory.js
+++ src/scripts/directory.js
@@ -2,17 +2,21 @@ import {db} from './init.js';
 
 const tabla = document.querySelector('#tabla');
 
+//Crear nuevo espacio cuando se añade un nuevo abogado
 function renderLawyer(doc){
     let li = document.createElement('li');
     let name = document.createElement('span');
     let phone = document.createElement ('span');
+    let perfilbtn = document.createElement ('button');
 
     li.setAttribute('data-id', doc.id);
     name.textContent = doc.data().name + " " + doc.data().lastName;
     phone.textContent = doc.data().phone;
+    perfilbtn.textContent = 'Ver más';
 
     li.appendChild(name);
     li.appendChild(phone);
+    li.appendChild(perfilbtn);
 
     tabla.appendChild(li);
 }
