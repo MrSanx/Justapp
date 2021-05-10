70fe5895fae4c317dd2142a8958a60b990fbd286
Nicolas Suarez <nicolasuarez24@gmail.com>
2021-05-10 14:31:45 -0500

    modal reviews

--- src/scripts/reviews.js
+++ src/scripts/reviews.js
@@ -22,3 +22,26 @@ btn.onclick = function() {
 span.onclick = function() {
   modal.style.display = "none";
 }
+
+function addReviewAbogado ()
+{
+    id = document.getElementById("abogado");
+
+    db.collection("lawyers").get().then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+           var id2 = doc.id;
+           if(id==id2)
+           {
+            var abogado = document.getElementById("abogado");
+            var review = document.getElementById("review");
+            var arreglo = new reviews (abogado,review);
+            reviews[cantReviews]=arreglo;
+            cantReviews=cantReviews+1;
+            db.collection('reviews').add(review);
+           }
+           else {
+            document.getElementById("dato1").innerHTML="No se pudo encontrar su abogado";
+           }
+        });
+    });
+}
