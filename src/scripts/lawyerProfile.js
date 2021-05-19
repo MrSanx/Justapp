acbe15d29c429fb96bef966dc50c9afd65e08fd6
Nicolas Suarez <nicolasuarez24@gmail.com>
2021-05-19 16:41:35 -0500

    añadir la reseña al perfil del abogado

--- src/scripts/lawyerProfile.js
+++ src/scripts/lawyerProfile.js
@@ -5,6 +5,11 @@ const position = document.querySelector('#lawyer-position');
 const city = document.querySelector('#lawyer-city');
 const workArea = document.querySelector('#lawyer-workArea');
 const description = document.querySelector('#lawyer-description');
+const reviewName = document.querySelector('#review-lawyer-name');
+const reviewCity = document.querySelector('#review-lawyer-city');
+const reviewPositionWorkarea = document.querySelector('#review-lawyer-position-workArea');
+
+let tabla = document.querySelector('#tabla');
 
 export const setupProfile = (id) => {
     db.collection('lawyers').doc(id).get().then(doc => {
@@ -16,10 +21,41 @@ export const setupProfile = (id) => {
         const htmlDescription = `${doc.data().description}`;
 
         name.innerHTML = htmlName;
+        reviewName.innerHTML = htmlName;
+        reviewCity.innerHTML = htmlCity;
+        reviewPositionWorkarea.innerHTML = htmlPosition +" / "+ `${doc.data().workArea}`;
         position.innerHTML = htmlPosition;
         city.innerHTML = htmlCity;
         workArea.innerHTML = htmlWorkArea;
         description.innerHTML = htmlDescription;       
 
     })
+}
+
+db.collection("reviews").onSnapshot((querySnapshot) => {
+    tabla.innerHTML='';
+    querySnapshot.forEach((doc) => {
+        renderReview(doc);
+    });
+})
+
+function renderReview(doc){
+    let li = document.createElement('li');
+    let title = document.createElement('span')
+    let stars = document.createElement('span');
+    let type = document.createElement ('span');
+    let experience = document.createElement ('span');
+
+    li.setAttribute('data-id', doc.id);
+    title.textContent = doc.data().title;
+    stars.textContent = "Calificación: " + doc.data().stars;
+    type.textContent = doc.data().type;
+    experience.textContent = doc.data().experience;
+
+    li.appendChild(title);
+    li.appendChild(stars);
+    li.appendChild(type);
+    li.appendChild(experience);
+
+    tabla.appendChild(li);
 }
