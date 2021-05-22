449328417ab8cc34092c024a566ed2592ae0897d
Nicolas Suarez <nicolasuarez24@gmail.com>
2021-05-22 17:47:10 -0500

    retirar nav de donde no tiene sentido que esté

--- src/scripts/logout.js
+++ src/scripts/logout.js
@@ -5,4 +5,5 @@ const logout = document.querySelector('#logout');
 logout.addEventListener('click', (e) => {
     e.preventDefault();
     auth.signOut();
+    window.location.href = '../webapp/index.html';
 });
